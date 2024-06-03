ac35012e6246e958684b287635bcecee7acdabd3
shinjimomoi <shinjimomoi@gmail.com>
2024-06-03 23:58:58 +0900

    reinitialize repo

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,8 +5,8 @@ import Game from './Game';
 const Page: React.FC = () => {
   return (
     <div className='container'>
-      <h1>Card Generator</h1>
-      <p>You can play once a day to get a card.</p>
+      <h1>Pokeslot</h1>
+      <p>You can play and get a card!</p>
       <Game />
     </div>
   );
